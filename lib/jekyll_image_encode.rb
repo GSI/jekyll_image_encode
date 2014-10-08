564f8896b3047ab6f793aee484cb7b4482ce17ea
Gregor Sturm <mail@gregor-sturm.de>
2014-10-08 12:34:21 +0200

    cleaned up files. removed overhead due to double call of open()

--- lib/jekyll_image_encode.rb
+++ lib/jekyll_image_encode.rb
@@ -31,18 +31,19 @@ module Jekyll
         require 'base64'
         
         encoded_image = ''
-        data_type = MimeMagic.by_magic(open(@url))
+        image_handle = open(@url)
 
         if self.cached_base64_codes.has_key? @url
           encoded_image = self.cached_base64_codes[@url]
         else
           # p "Caching #{@url} as local base64 string ..."
-          open(@url) do |image|
-            encoded_image = Base64.strict_encode64(image.read)
-          end
+          encoded_image = Base64.strict_encode64(image_handle.read)
           self.cached_base64_codes.merge!(@url => encoded_image)
         end
         
+        data_type = MimeMagic.by_magic(image_handle)
+        image_handle.close
+
         "data:#{data_type};base64, #{encoded_image}"
       end
     end
